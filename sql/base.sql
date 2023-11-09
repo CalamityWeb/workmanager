3415bf35b0f39f5fde13a8eb3a577367a0dd17f0
tokrist <tokrist15@gmail.com>
2023-11-09 08:39:32 +0100

    Database update
    Signed-off-by: tokrist <tokrist15@gmail.com>

--- sql/base.sql
+++ sql/base.sql
@@ -1,72 +1,62 @@
+CREATE TABLE auth_groups
+(
+    code        VARCHAR(255)                       NOT NULL
+        PRIMARY KEY,
+    groupName   VARCHAR(255)                       NOT NULL,
+    description TEXT                               NULL,
+    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at  DATETIME                           NULL ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE auth_items
+(
+    item        VARCHAR(255)                       NOT NULL
+        PRIMARY KEY,
+    description TEXT                               NULL,
+    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at  DATETIME                           NULL ON UPDATE CURRENT_TIMESTAMP
+);
+
 CREATE TABLE auth_assignments
 (
-    code       VARCHAR(255)                         NOT NULL,
-    item       VARCHAR(255)                         NOT NULL,
-    type       SET ('ADMIN', 'PUBLIC')              NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP() NOT NULL,
-    updated_at DATETIME                             NULL ON UPDATE CURRENT_TIMESTAMP()
+    code       VARCHAR(255)                       NOT NULL,
+    item       VARCHAR(255)                       NOT NULL,
+    type       SET ('ADMIN', 'PUBLIC')            NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at DATETIME                           NULL ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT auth_assignments_auth_groups_code_fk
+        FOREIGN KEY (code) REFERENCES auth_groups (code)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT auth_assignments_auth_items_item_fk
+        FOREIGN KEY (item) REFERENCES auth_items (item)
+            ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE INDEX auth_assignments_type_index
     ON auth_assignments (type);
 
-CREATE TABLE auth_groups
-(
-    code        VARCHAR(255)                         NOT NULL,
-    groupName   VARCHAR(255)                         NOT NULL,
-    description TEXT                                 NULL,
-    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP() NOT NULL,
-    updated_at  DATETIME                             NULL ON UPDATE CURRENT_TIMESTAMP()
-);
-
-ALTER TABLE auth_groups
-    ADD PRIMARY KEY (code);
-
-ALTER TABLE auth_assignments
-    ADD CONSTRAINT auth_assignments_auth_groups_code_fk
-        FOREIGN KEY (code) REFERENCES auth_groups (code)
-            ON UPDATE CASCADE ON DELETE CASCADE;
-
-CREATE TABLE auth_items
-(
-    item        VARCHAR(255)                         NOT NULL,
-    description TEXT                                 NULL,
-    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP() NOT NULL,
-    updated_at  DATETIME                             NULL ON UPDATE CURRENT_TIMESTAMP()
-);
-
-ALTER TABLE auth_items
-    ADD PRIMARY KEY (item);
-
-ALTER TABLE auth_assignments
-    ADD CONSTRAINT auth_assignments_auth_items_item_fk
-        FOREIGN KEY (item) REFERENCES auth_items (item)
-            ON UPDATE CASCADE ON DELETE CASCADE;
-
-CREATE TABLE role_groups
-(
-    roleId     INT                                  NOT NULL,
-    groupCode  VARCHAR(255)                         NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP() NOT NULL,
-    updated_at DATETIME                             NULL ON UPDATE CURRENT_TIMESTAMP()
-);
-
 CREATE TABLE roles
 (
     id          INT AUTO_INCREMENT
         PRIMARY KEY,
-    roleName    VARCHAR(255)                         NOT NULL,
-    description TEXT                                 NULL,
-    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP() NOT NULL,
-    updated_at  DATETIME                             NULL ON UPDATE CURRENT_TIMESTAMP()
+    roleName    VARCHAR(255)                       NOT NULL,
+    description TEXT                               NULL,
+    created_at  DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at  DATETIME                           NULL ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE user_roles
+CREATE TABLE role_groups
 (
-    userId     INT                                  NOT NULL,
-    roleId     INT                                  NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP() NOT NULL,
-    updated_at DATETIME                             NULL ON UPDATE CURRENT_TIMESTAMP()
+    roleId     INT                                NOT NULL,
+    groupCode  VARCHAR(255)                       NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at DATETIME                           NULL ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT role_groups_auth_groups_code_fk
+        FOREIGN KEY (groupCode) REFERENCES auth_groups (code)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT role_groups_roles_id_fk
+        FOREIGN KEY (roleId) REFERENCES roles (id)
+            ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE users
@@ -77,8 +67,33 @@ CREATE TABLE users
     firstName       VARCHAR(255)                         NOT NULL,
     lastName        VARCHAR(255)                         NOT NULL,
     password        VARCHAR(255)                         NOT NULL,
-    email_confirmed TINYINT(1)                           NOT NULL,
-    created_at      DATETIME DEFAULT CURRENT_TIMESTAMP() NOT NULL,
-    updated_at      DATETIME                             NULL ON UPDATE CURRENT_TIMESTAMP()
+    email_confirmed TINYINT(1) DEFAULT 0                 NOT NULL,
+    created_at      DATETIME   DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at      DATETIME                             NULL ON UPDATE CURRENT_TIMESTAMP
 );
 
+CREATE TABLE reset_tokens
+(
+    token        VARCHAR(64)                        NOT NULL
+        PRIMARY KEY,
+    userId       INT                                NOT NULL,
+    created_at   DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    completed_at DATETIME                           NULL,
+    CONSTRAINT reset_tokens_users_id_fk
+        FOREIGN KEY (userId) REFERENCES users (id)
+            ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE user_roles
+(
+    userId     INT                                NOT NULL,
+    roleId     INT                                NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at DATETIME                           NULL ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT user_roles_roles_id_fk
+        FOREIGN KEY (roleId) REFERENCES roles (id)
+            ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT user_roles_users_id_fk
+        FOREIGN KEY (userId) REFERENCES users (id)
+            ON UPDATE CASCADE ON DELETE CASCADE
+);
